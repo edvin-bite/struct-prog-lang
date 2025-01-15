65372e842b544139a131ea2afbd7bcdc02f5a429
edvin-bite <snackpackbpg@gmail.com>
2025-01-15 21:25:06 +0000

    save1

---
+++ topic-01-integers/eval.py
@@ -0,0 +1,28 @@
+def eval(s):
+    for c in s:
+        assert s in "-0123456789"
+    n = 0
+    if s[0] == '-':
+        sign = -1
+        s = s[1:] ## drop first char
+    else:
+        sign = 1
+    assert len(s) > 0
+    while len(s) > 0:
+        n = n * 10 + ord(s[0]) - ord("0")
+        s = s[1:]
+    return n * sign
+
+def test_eval():
+    """ test eval """
+    print("testing eval()")
+    assert eval("0") == 0, "Expect 0 to be 0"
+    assert eval("1") == 1
+    assert eval("99") == 99
+    assert eval("1099") == 1099
+    assert eval("0001") == 1
+    assert eval("-99") == -99
+
+if __name__ == "__main__":
+    test_eval()
+    print("done.")
